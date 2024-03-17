1611f53b78ee7d1835e68bd19117e4c20700b4b7
Max Berger <max@berger.name>
2024-03-17 15:14:09 +0100

    Add mypy

--- dagger/src/main.py
+++ dagger/src/main.py
@@ -20,18 +20,21 @@ from dagger import dag, function, object_type
 @object_type
 class Daggerverse:
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
-
-    @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
+    async def mypy(self, src: dagger.Directory) -> str:
+        return (
+            await (
+                dag.container()
+                .from_("python:alpine")
+                .with_mounted_directory("/mnt", src)
+                .with_workdir("/mnt")
+                .with_exec(["pip", "install", "mypy"])
+                .with_exec(
+                    ["apk", "add", "--no-cache", "gcc", "libc-dev", "linux-headers"]
+                )
+                .with_exec(["python", "--version"])
+                # .with_exec(["pip","install","wheel"])
+                # .with_exec(["pip","install","-U","-r","test-requirements.txt"])
+                .with_exec(["pip", "install", "mypy"])
+                .with_exec(["mypy", "--non-interactive", "--install-types", "."])
+            ).stdout()
         )
